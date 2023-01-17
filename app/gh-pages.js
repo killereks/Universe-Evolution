6fbf425064bc49615b771b0c78c8a452efeac69e
Oskar <oskaroramus@gmail.com>
2023-01-17 01:34:02 +0000

    initial gh-pages commit

---
+++ app/gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/killereks/Universe-Evolution', // Update to point to your repository  
+        user: {
+            name: 'Oskar', // update to use your name
+            email: 'Your Email address' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
